1dddaaf19d6e3a21f206bfb7e98120e11250419b
ahmed <ahmed@ahmeds-Mac.local>
2016-11-25 02:20:05 +0200

    adding Alamofire cocoapod lib

---
+++ Pods/Target Support Files/Pods-weathertest1/Pods-weathertest1-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_weathertest1VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_weathertest1VersionString[];
+
